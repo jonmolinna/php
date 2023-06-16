ae6e09b94c56b9c3b6ba68c48703dace8e69e40e
jonmolinna <jmolina2624@gmail.com>
2023-06-16 08:14:42 -0500

    Cookie

---
+++ 1-basico/6-pedir_idioma.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="6-grabar_cookie.php?idioma=es">Version en español</a>
+    <br /><br />
+    <a href="6-grabar_cookie.php?idioma=en">Version en ingles</a>
+    
+</body>
+</html>
